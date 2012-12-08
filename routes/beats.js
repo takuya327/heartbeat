c88292b7ec258f841cf90d26c381596eb7b6b002
Fujii Takuya <takuya327@gmail.com>
2012-12-08 16:05:08 +0900

    save!

--- routes/beats.js
+++ routes/beats.js
@@ -4,9 +4,12 @@ exports.update = function(req, res){
   name = req.params.name;
   console.log("beat update: name: " + name);
   key = "heartbeat:user:" + name
-
+  
   redis.exists( key, function(err,value) {
-    redis.hset( key, "name", name);
+    
+    if( name ) {
+      redis.hset( key, "name", name);
+    }
     
     if( !err ) {
       console.log("name:" + name);
@@ -14,9 +17,9 @@ exports.update = function(req, res){
       beatY = req.body.beatY || req.query.beatY
       beatZ = req.body.beatZ || req.query.beatZ
       
-      redis.hset( key, "beatX", beatX);
-      redis.hset( key, "beatY", beatY);
-      redis.hset( key, "beatZ", beatZ);
+      if( beatX ) { redis.hset( key, "beatX", beatX); }
+      if( beatY ) { redis.hset( key, "beatY", beatY); }
+      if( beatZ ) { redis.hset( key, "beatZ", beatZ); }
     }
     res.redirect("/users");
   });
