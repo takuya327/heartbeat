690321313eee787a60aa2a653768eeba892d86c1
Fujii Takuya <takuya327@gmail.com>
2012-12-08 16:01:20 +0900

    save!

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var routes  = require('./routes');
-var users = require('./routes/users');
-var beats = require('./routes/beats');
+var users = {};
+var beats = {};
 var app = express(express.logger());
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
+var redis = require('redis-url').connect(process.env.REDISTOGO_URL);
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
@@ -23,6 +24,68 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+users.index = function(req, res){
+  redis.keys( "heartbeat:user:*", function(err,value){
+    var keys = value || [];
+    console.log( "keys: " + keys );
+    var users = [];
+    
+    for( var i = 0, len = keys.length; i < len; i++ ) {
+      var key = keys[i];
+      console.log( "key: " + key );
+      redis.hgetall( key, function(err,value){
+        if( !err ) {
+          console.log("value: " + JSON.stringify(value) );
+          users.push( value );
+        }
+        
+        if( users.length == len ) {
+          res.render("users/index", { users: users } );
+        }
+      });
+    }
+    
+  })
+}
+
+users.create = function(req,res){
+  name = req.body.name;
+  key = "heartbeat:user:" + name
+  
+  redis.exists( key, function(err,value) {
+    if( !err ) {
+      console.log("name:" + name);
+      redis.hset( key, "name", name);
+      redis.hsetnx( key, "beatX", 0 );
+      redis.hsetnx( key, "beatY", 0 );
+      redis.hsetnx( key, "beatZ", 0 );
+    }
+    res.redirect("/users");
+  });
+}
+
+beats.update = function(req, res){
+  name = req.params.name;
+  console.log("beat update: name: " + name);
+  key = "heartbeat:user:" + name
+
+  redis.exists( key, function(err,value) {
+    redis.hset( key, "name", name);
+    
+    if( !err ) {
+      console.log("name:" + name);
+      beatX = req.body.beatX || req.query.beatX
+      beatY = req.body.beatY || req.query.beatY
+      beatZ = req.body.beatZ || req.query.beatZ
+      
+      redis.hset( key, "beatX", beatX);
+      redis.hset( key, "beatY", beatY);
+      redis.hset( key, "beatZ", beatZ);
+    }
+    res.redirect("/users");
+  });
+}
+
 app.get('/', routes.index );
 app.get('/users', users.index );
 app.post('/users', users.create );
@@ -39,4 +102,3 @@ io.sockets.on('connection', function (socket) {
     console.log(data);
   });
 });
-
